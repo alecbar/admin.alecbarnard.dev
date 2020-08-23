e12460315ae9646aa81ce1c1360f6a9e9f15ddd2
Alec Barnard <alecbarnard@live.com>
2020-08-22 20:07:22 -0700

    Trying to troubleshoot mongo

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config()
 
 // MonogoDB Connected
 const mongoose = require('mongoose')
+console.log("MongoURI: ", process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err) =>{
   if(err){
     console.log(err)
